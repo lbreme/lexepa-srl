8759a02d9eae9e78fe62715235970569b20ceafc
Lorenzo <lorenzo.breme@outlook.it>
2021-05-13 10:34:51 +0200

    Example of a serialized PHP array

--- class-example-srl.php
+++ class-example-srl.php
@@ -5,25 +5,23 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Breme\Lexepa\srl\Lexepa_Srl_Abstract;
 use Breme\Lexepa\srl\Lexepa_Srl;
 
-class MyClass
-{
-	private   $my_private   = 'This is my private attribute';
+$my_array = array(
+	'key1' => 'This is my first value',
+	'key2' => 'This is my second value',
+	'key3' => 20,
+	'key4' => 45.98,
+	'key4' => true,
+	'key5' => null
+);
 
-	protected $my_protected = 'This is my protected attribute';
-
-	protected $my_array     = array(
-		'key1' => 'This is my first value',
-		'key2' => 'This is my second value',
-		'key3' => 20
-	);
-}
-
-$myObject = new MyClass();
-
-$myObjectSerialized = serialize( $myObject );
+$myArraySerialized = serialize( $my_array );
 
 class Example_Srl extends Lexepa_Srl_Abstract
 {
+	public function begin_parsing( $string, $offset ) {
+		echo 'String serialized: ' . $string . '<br />';
+	}
+
 	public function string_length( $string_length, $offset )
 	{
 		echo 'String length: ' . $string_length . '<br />';
@@ -37,6 +35,22 @@ class Example_Srl extends Lexepa_Srl_Abstract
 	public function integer_value( $integer_value, $offset )
 	{
 		echo 'Integer value: ' . $integer_value . '<br />';
+	}
+
+	public function decimal_value( $decimal_value, $offset ) {
+		echo 'Decimal value: ' . $decimal_value . '<br />';
+	}
+
+	public function boolean_value( $boolean_value, $offset ) {
+		echo 'Boolean value: ' . $boolean_value . '<br />';
+	}
+
+	public function null_value() {
+		echo 'Null value found' . '<br />';
+	}
+
+	public function array_items_num( $items_num, $offset ) {
+		echo 'Number of items of the array: ' . $items_num . '<br />';
 	}
 
 	public function end_parsing( $parse_result )
@@ -55,7 +69,7 @@ class Example_Srl extends Lexepa_Srl_Abstract
 }
 
 $example_srl = new Example_Srl();
-$lexepa_srl  = new Lexepa_Srl( $example_srl, $myObjectSerialized );
+$lexepa_srl  = new Lexepa_Srl( $example_srl, $myArraySerialized );
 
 $lexepa_srl->parse_srl();
 
